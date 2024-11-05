4c84ae98dd2cd997ad61880feb66c5fe12cb4e50
fadilsid <fadil@tridz.com>
2024-11-05 07:03:32 +0000

    first commit

---
+++ meta_leads/meta_leads/doctype/meta_settings/meta_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Upscape Technologies and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class MetaSettings(Document):
+	pass
---
+++ meta_leads/meta_leads/doctype/meta_settings/test_meta_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Upscape Technologies and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMetaSettings(FrappeTestCase):
+	pass
